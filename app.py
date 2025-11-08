569a95eb80df71338993a7ea5c81625b430c4ad8
George Zacharias <zachariasg7@hotmail.gr>
2025-11-08 13:44:59 +0200

    optimized more , added sqrt

--- app.py
+++ app.py
@@ -17,34 +17,43 @@ class Calculator():
 
     def div(self):
         if self.y == 0:
-            return ("invalid number try again")
+            return "invalid number try again"
         return self.x/self.y
 
+    def sqrt(self):
+        if self.x < 0:
+            return "cant find the sqrt of a negative number"
+        return math.sqrt(self.x)
 
-name = str = input("enter your name ")
+
+name = input("enter your name ")
 print(f"hello {name} ")
 
-action = str = input("you want to do add,sub,mul,div or exit? \n ").lower()
+action = input("you want to do add,sub,mul,div,sqrt or exit? \n ").lower()
 
 
 while (action != "exit"):
 
-    if action in ["add", "sub", "mul", "div"]:
-
-        x = int(input("give me first number "))
-        y = int(input("give me second number "))
-        calc = Calculator(x, y)
-        if action == "add":
-            print(f"Result {calc.add()}\n")
-        elif action == "sub":
-            print(f"Result: {calc.sub()}\n")
-        elif action == "mul":
-            print(f"Result: {calc.mul()}\n")
-        elif action == "div":
-            print(f"Result: {calc.div()}\n")
+    if action in ["add", "sub", "mul", "div", "sqrt"]:
+        if (action == "sqrt"):
+            x = int(input("give me the number "))
+            calc = Calculator(x, 0)
+            print(f"Result:  {calc.sqrt()}")
+        else:
+            x = float(input("give me first number "))
+            y = float(input("give me second number "))
+            calc = Calculator(x, y)
+            if action == "add":
+                print(f"Result {calc.add()}\n")
+            elif action == "sub":
+                print(f"Result: {calc.sub()}\n")
+            elif action == "mul":
+                print(f"Result: {calc.mul()}\n")
+            elif action == "div":
+                print(f"Result: {calc.div()}\n")
     else:
         print("unknown command.Try again")
-    action = input("you want to do add,sub,mul,div or exit?").lower()
+    action = input("you want to do add,sub,mul,div,sqrt or exit?").lower()
 
 
 print("\nexiting program...")
