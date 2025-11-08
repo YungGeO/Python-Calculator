ced73722588487dea7b856cd19f037e34a1a0e14
George Zacharias <zachariasg7@hotmail.gr>
2025-11-08 13:21:03 +0200

    loop ready

--- app.py
+++ app.py
@@ -24,3 +24,14 @@ while (action != "exit"):
         X = int(input("give me first number "))
         Y = int(input("give me second number "))
         calc.add(X, Y)
+    elif action == "sub":
+        X = int(input("give me first number "))
+        Y = int(input("give me second number "))
+        calc.sub(X, Y)
+    elif action == "mul":
+        X = int(input("give me first number "))
+        Y = int(input("give me second number "))
+        calc.mul(X, Y)
+    else:
+        print("unknown command.Try again")
+    action = input("you want to do add,sub,mul or exit?").lower()
