c5f88a7b16d4e0124ae3f7a885f03e803192a19c
George Zacharias <zachariasg7@hotmail.gr>
2025-11-08 13:37:15 +0200

    division optimized and check

--- app.py
+++ app.py
@@ -15,16 +15,21 @@ class Calculator():
     def mul(self):
         return self.x * self.y
 
+    def div(self):
+        if self.y == 0:
+            return ("invalid number try again")
+        return self.x/self.y
+
 
 name = str = input("enter your name ")
 print(f"hello {name} ")
 
-action = str = input("you want to do add,sub,mul or exit? \n ").lower()
+action = str = input("you want to do add,sub,mul,div or exit? \n ").lower()
 
 
 while (action != "exit"):
 
-    if action in ["add", "sub", "mul"]:
+    if action in ["add", "sub", "mul", "div"]:
 
         x = int(input("give me first number "))
         y = int(input("give me second number "))
@@ -34,10 +39,12 @@ while (action != "exit"):
         elif action == "sub":
             print(f"Result: {calc.sub()}\n")
         elif action == "mul":
-            print(f"Result: {calc.sub()}\n")
+            print(f"Result: {calc.mul()}\n")
+        elif action == "div":
+            print(f"Result: {calc.div()}\n")
     else:
         print("unknown command.Try again")
-    action = input("you want to do add,sub,mul or exit?").lower()
+    action = input("you want to do add,sub,mul,div or exit?").lower()
 
 
 print("\nexiting program...")
